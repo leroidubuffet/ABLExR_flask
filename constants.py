5e2f1606b321745db272530efda146fef94f8b75
Yago Bolívar <mr.bijou@gmail.com>
2023-08-02 20:04:00 +0200

    style(constants): fixed line length

--- constants.py
+++ constants.py
@@ -14,6 +14,7 @@ COLOR_PALETTE = ["#6497b1", "#ff6f69", "#011f4b"]
 # Error messages
 INVALID_PASSWORD = 'Invalid password. Please try again.'
 SESSION_ID_EXISTS = 'Session ID already exists. Please try a different ID.'
-SESSION_ID_NOT_EXIST = 'That session ID does not exist. Please try a different ID.'
+SESSION_ID_NOT_EXIST = 'That session ID does not exist. Please try \
+    a different ID.'
 SESSION_ID_MUST_BE_DIGIT = 'Session ID must be a 3 digit number.'
 INSERT_FOUR_DIGIT_NUMBER = 'Insert a 4 digit number please.'
