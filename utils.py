00ff619a4c8ac8a44aba1fbc4f47622bf5c75bc5
Yago Bolívar <mr.bijou@gmail.com>
2023-08-03 13:10:25 +0200

    refactor(utils): rename map_ethnicity function dictionary

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 def map_ethnicity(code):
 	ethnicity_mapping = {
-		1: 'Black',
-		2: 'Latino',
-		3: 'White'
+		'black': '1',
+		'latino': '2',
+		'white': '3'
 	}
 	return ethnicity_mapping.get(code, 'Unknown')
+
 
 def validate_session_id(session_id):
 	if not session_id.isdigit() or len(session_id) != 4:
