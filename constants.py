0ef9f647562ba99979ec1cecd83fafc47d127c7e
Yago Bolívar <mr.bijou@gmail.com>
2023-08-03 13:11:40 +0200

    style(config, constants): minor changes

--- constants.py
+++ constants.py
@@ -2,11 +2,14 @@
 CREDENTIALS_FILE = 'credentials.json'
 DOCUMENT_NAME = 'ABLExR-DATA'
 
+# Ethnicities
 ETHNICITY_MAPPING = {
     1: 'Black',
     2: 'Latino',
     3: 'White'
 }
+
+ETHNICITIES = ['random', 'black', 'latino', 'white']
 
 # Officer, Driver, Scene dots
 COLOR_PALETTE = ["#6497b1", "#ff6f69", "#011f4b"]
@@ -14,8 +17,9 @@ COLOR_PALETTE = ["#6497b1", "#ff6f69", "#011f4b"]
 # Error messages
 INVALID_PASSWORD = 'Invalid password. Please try again.'
 SESSION_ID_EXISTS = 'Session ID already exists. Please try a different ID.'
-SESSION_ID_NOT_EXIST = 'That session ID does not exist. Please try \
-    a different ID.'
+SESSION_ID_NOT_EXIST = (
+    'Session ID does not exist. Please try a different ID.'
+)
 SESSION_ID_MUST_BE_DIGIT = 'Session ID must be a 3 digit number.'
 INSERT_FOUR_DIGIT_NUMBER = 'Insert a 4 digit number please.'
 
